b76372845705022ae4241f1497ae55afb36fcd5f
Abelink2119 <abelink2119@Gmail.com>
2025-07-09 13:52:22 +0300

    data types

---
+++ C++/Data types.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int age;
+ age = 23;
+ string name ="Abel";
+
+  cout << "my name is : "<<name<< endl;
+  cout << "my age is : "<<age<< endl;
+  return 0;  
+}
