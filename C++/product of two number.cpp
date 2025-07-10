e99f38cdfe9c38f8e3b4a2d68dc6f0917d1acb9f
Abelink2119 <abelink2119@Gmail.com>
2025-07-10 12:05:12 +0300

    Update project

---
+++ C++/product of two number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, product;
+
+    cout << "Enter your first number: " << endl;
+    cin >> a;
+
+    cout << "Enter your second number: " << endl;
+    cin >> b;
+
+    product = a*b;
+
+    cout << "The product of two numbers is: " << product<< endl;
+
+    return 0;
+}
---
+++ C++/sum of two number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "Enter your first number: " << endl;
+    cin >> a;
+
+    cout << "Enter your second number: " << endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "The sum of two numbers is: " << sum << endl;
+
+    return 0;
+}
