779ea735b6e7cf4edff8305b887832e92a4c89a0
Abelink2119 <abelink2119@Gmail.com>
2025-07-11 14:04:41 +0300

    larger number

---
+++ C++/larger number using function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void findLarger(int num1, int num2) {
+    if (num1 > num2) {
+        cout << "Larger number is: " << num1 << endl;
+    } else if (num2 > num1) {
+        cout << "Larger number is: " << num2 << endl;
+    } else {
+        cout << "Both numbers are equal." << endl;
+    }
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Call the function
+    findLarger(num1, num2);
+
+    return 0;
+}
