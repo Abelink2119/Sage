d3a2cff723becda9f2141d3e575ce4cfc5d65190
Abelink2119 <abelink2119@Gmail.com>
2025-07-11 12:57:49 +0300

    vowel in array

---
+++ C++/vowel in array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+    int i = 0;
+
+    cout << "Vowels are: ";
+    while (i < 5) {
+        cout << vowels[i] << " ";
+        i++;
+    }
+
+    cout << endl;
+
+    return 0;
+}
