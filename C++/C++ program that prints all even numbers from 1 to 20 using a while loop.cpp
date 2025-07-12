f73262bad33afcbd00df7424cfe3bc217f350390
Abelink2119 <abelink2119@Gmail.com>
2025-07-12 22:09:32 +0300

    C++ program that prints all even numbers from 1 to 20 using a while loop

---
+++ C++/C++ program that prints all even numbers from 1 to 20 using a while loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    cout << "Even numbers from 1 to 20:" << endl;
+
+    while (i <= 20) {
+        if (i % 2 == 0) {
+            cout << i << endl;
+        }
+        i++;
+    }
+
+    return 0;
+}
